8b6fe482b9bafd5454ddf6cb1c70c80aa98f52a7
Fertern <ferternever@gmail.com>
2019-10-09 18:32:45 +0300

    upd app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './styles/App.css';
+import './dist/App.css';
 import Header from './components/Header';
 import Nav from './components/Nav';
 import Profile from './components/Profile';
